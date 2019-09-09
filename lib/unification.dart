e0f879cac5a3f43c3c0bd37d5753fffc1208842f
buergerling <michael.buergerling@gmail.com>
2019-09-09 11:46:09 +0200

    ...

--- lib/unification.dart
+++ lib/unification.dart
@@ -1,12 +1,10 @@
 library unification;
-
+/*
 import "package:tailcalls/tailcalls.dart";
-
 import 'src/utils.dart';
-import 'src/terms.dart';
-
 export 'src/utils.dart';
-export 'src/terms.dart';
-
+import 'src/terms.dart';
+export 'src/terms.dart'; 
+*/
 import 'src/unification1.dart' show Unification, Term, Var, Tupl;
 export 'src/unification1.dart' show Unification, Term, Var, Tupl;
