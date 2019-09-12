e56dec5b3dcb999019a9ab9f8bd55e5a5e4a6b9f
buergerling <michael.buergerling@gmail.com>
2019-09-12 10:05:21 +0200

    ...

--- lib/unification.dart
+++ lib/unification.dart
@@ -1,4 +1,4 @@
 library unification;
 
 //import 'src/unification3.dart' show Unification, Term, Var, Tupl;
-export 'src/unification1.dart' show UnificationR, Term, Var, Tupl;
+export 'src/unification3.dart' show Unification, Term, Var, Tupl;
